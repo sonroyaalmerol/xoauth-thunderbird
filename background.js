ee7d003a592b6306bc613aeec836b013fba6e062
Son Roy Almerol <sralmerol@sgl.com>
2026-01-30 16:07:05 -0500

    set keepalive for background script v3

--- background.js
+++ background.js
@@ -38,6 +38,12 @@ async function registerExistingAccounts() {
   }
 }
 
+function keep_alive() {
+  let getting = browser.runtime.getPlatformInfo();
+  setTimeout(keep_alive, "20000");
+}
+
+keep_alive();
 registerExistingAccounts();
 
 messenger.accounts.onCreated.addListener((accountId, account) => {
